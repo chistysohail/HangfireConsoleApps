914e6812866a2f4a60efc7c32dec0b5193a89d77
chisty sohel <chistysohail@gmail.com>
2024-07-31 12:03:08 -0400

    updated to AllowAllAuthorizationFilter

--- HangfireServerApp/Program.cs
+++ HangfireServerApp/Program.cs
@@ -51,36 +51,36 @@ namespace HangfireServerApp
                         // Get the connection string
                         var connectionString = configuration.GetConnectionString("HangfireConnection");
 
-                        // Read Hangfire settings from configuration
-                        var hangfireSettings = configuration.GetSection("HangfireSettings");
-                        var username = hangfireSettings.GetValue<string>("Username");
-                        var password = hangfireSettings.GetValue<string>("Password");
-                        var requireSsl = hangfireSettings.GetValue<bool>("RequireSsl");
-                        var sslRedirect = hangfireSettings.GetValue<bool>("SslRedirect");
-                        var loginCaseSensitive = hangfireSettings.GetValue<bool>("LoginCaseSensitive");
+                        //// Read Hangfire settings from configuration
+                        //var hangfireSettings = configuration.GetSection("HangfireSettings");
+                        //var username = hangfireSettings.GetValue<string>("Username");
+                        //var password = hangfireSettings.GetValue<string>("Password");
+                        //var requireSsl = hangfireSettings.GetValue<bool>("RequireSsl");
+                        //var sslRedirect = hangfireSettings.GetValue<bool>("SslRedirect");
+                        //var loginCaseSensitive = hangfireSettings.GetValue<bool>("LoginCaseSensitive");
 
-                        // Configure Hangfire to use the connection string
-                        GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
+                        //// Configure Hangfire to use the connection string
+                        //GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
 
-                        // Configure Hangfire dashboard with Basic Authentication
-                        var options = new DashboardOptions
-                        {
-                            Authorization = new[]
-                            {
-                                new BasicAuthAuthorizationFilter(
-                                    username: username,
-                                    password: password,
-                                    requireSsl: requireSsl,
-                                    sslRedirect: sslRedirect,
-                                    loginCaseSensitive: loginCaseSensitive)
-                            }
-                        };
-
-                        // Configure Hangfire dashboard with AllowAllAuthorizationFilter
+                        //// Configure Hangfire dashboard with Basic Authentication
                         //var options = new DashboardOptions
                         //{
-                        //    Authorization = new[] { new AllowAllAuthorizationFilter() }
+                        //    Authorization = new[]
+                        //    {
+                        //        new BasicAuthAuthorizationFilter(
+                        //            username: username,
+                        //            password: password,
+                        //            requireSsl: requireSsl,
+                        //            sslRedirect: sslRedirect,
+                        //            loginCaseSensitive: loginCaseSensitive)
+                        //    }
                         //};
+
+                        //Configure Hangfire dashboard with AllowAllAuthorizationFilter
+                       var options = new DashboardOptions
+                       {
+                           Authorization = new[] { new AllowAllAuthorizationFilter() }
+                       };
 
                         app.UseHangfireDashboard("/hangfire", options);
 
@@ -107,58 +107,58 @@ public class AllowAllAuthorizationFilter : Hangfire.Dashboard.IDashboardAuthoriz
     }
 
 
-public class BasicAuthAuthorizationFilter : IDashboardAuthorizationFilter
-    {
-        private readonly string _username;
-        private readonly string _password;
-        private readonly bool _requireSsl;
-        private readonly bool _sslRedirect;
-        private readonly bool _loginCaseSensitive;
+//public class BasicAuthAuthorizationFilter : IDashboardAuthorizationFilter
+//    {
+//        private readonly string _username;
+//        private readonly string _password;
+//        private readonly bool _requireSsl;
+//        private readonly bool _sslRedirect;
+//        private readonly bool _loginCaseSensitive;
 
-        public BasicAuthAuthorizationFilter(string username, string password, bool requireSsl = false, bool sslRedirect = false, bool loginCaseSensitive = true)
-        {
-            _username = username;
-            _password = password;
-            _requireSsl = requireSsl;
-            _sslRedirect = sslRedirect;
-            _loginCaseSensitive = loginCaseSensitive;
-        }
+//        public BasicAuthAuthorizationFilter(string username, string password, bool requireSsl = false, bool sslRedirect = false, bool loginCaseSensitive = true)
+//        {
+//            _username = username;
+//            _password = password;
+//            _requireSsl = requireSsl;
+//            _sslRedirect = sslRedirect;
+//            _loginCaseSensitive = loginCaseSensitive;
+//        }
 
-        public bool Authorize(DashboardContext context)
-        {
-            var httpContext = context.GetHttpContext();
+//        public bool Authorize(DashboardContext context)
+//        {
+//            var httpContext = context.GetHttpContext();
 
-            if (_requireSsl && !httpContext.Request.IsHttps)
-            {
-                if (_sslRedirect)
-                {
-                    var withHttps = "https://" + httpContext.Request.Host + httpContext.Request.Path;
-                    httpContext.Response.Redirect(withHttps);
-                }
+//            if (_requireSsl && !httpContext.Request.IsHttps)
+//            {
+//                if (_sslRedirect)
+//                {
+//                    var withHttps = "https://" + httpContext.Request.Host + httpContext.Request.Path;
+//                    httpContext.Response.Redirect(withHttps);
+//                }
 
-                return false;
-            }
+//                return false;
+//            }
 
-            var authHeader = httpContext.Request.Headers["Authorization"];
+//            var authHeader = httpContext.Request.Headers["Authorization"];
 
-            if (authHeader.ToString().StartsWith("Basic "))
-            {
-                var encodedUsernamePassword = authHeader.ToString().Substring("Basic ".Length).Trim();
-                var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
-                var username = decodedUsernamePassword.Split(':')[0];
-                var password = decodedUsernamePassword.Split(':')[1];
+//            if (authHeader.ToString().StartsWith("Basic "))
+//            {
+//                var encodedUsernamePassword = authHeader.ToString().Substring("Basic ".Length).Trim();
+//                var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+//                var username = decodedUsernamePassword.Split(':')[0];
+//                var password = decodedUsernamePassword.Split(':')[1];
 
-                if ((_loginCaseSensitive ? username == _username : username.Equals(_username, StringComparison.OrdinalIgnoreCase)) && password == _password)
-                {
-                    return true;
-                }
-            }
+//                if ((_loginCaseSensitive ? username == _username : username.Equals(_username, StringComparison.OrdinalIgnoreCase)) && password == _password)
+//                {
+//                    return true;
+//                }
+//            }
 
-            // Return 401 if the credentials are incorrect or not provided
-            httpContext.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Hangfire Dashboard\"";
-            httpContext.Response.StatusCode = 401;
-            return false;
-        }
-    }
+//            // Return 401 if the credentials are incorrect or not provided
+//            httpContext.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Hangfire Dashboard\"";
+//            httpContext.Response.StatusCode = 401;
+//            return false;
+//       }
+//    }
 
 
